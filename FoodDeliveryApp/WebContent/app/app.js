262d4244d14826f2a9f921efd3609ac55d3240f8
jovicpetra <jovic.petra@uns.ac.rs>
2021-08-10 14:50:34 +0200

    folder added

--- FoodDeliveryApp/WebContent/app/app.js
+++ FoodDeliveryApp/WebContent/app/app.js
@@ -9,3 +9,23 @@ let vue = new Vue({
             .then((response) => (this.restaurants = response.data));
     },
 });
+
+const RestaurantComponent = { template: '<view-restaurants></view-restaurants>'}
+const RegistrationComponent = { template: '<app-register></app-register>'}
+const LoginComponent = { template: '<app-login></app-login>'}
+
+const router = new VueRouter({
+	  mode: 'hash',
+	  routes: [
+		  { path: '/', component: RestaurantComponent},
+		  { path: '/restaurants', component: RestaurantComponent},
+		  { path: '/register', component: RegistrationComponent},
+		  { path: '/login', component: LoginComponent}
+		  
+	  ]
+});
+
+var app = new Vue({
+	router,
+	el: '#application'
+});
