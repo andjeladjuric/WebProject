027cdf35b78ed6a1805c8974677e88438d6bd1cd
Anđela Đurić <andjeladjuric@uns.ac.rs>
2021-08-12 02:27:59 +0200

    Fixed restaurants view

--- FoodDeliveryApp/WebContent/app/components/allRestaurants.js
+++ FoodDeliveryApp/WebContent/app/components/allRestaurants.js
@@ -2,6 +2,9 @@ Vue.component("all-restaurants", {
     data: function () {
         return {
             restaurants: [],
+            showSearch: false,
+            showFilter: false,
+            showSort: false,
         };
     },
     template: `
@@ -65,100 +68,106 @@ Vue.component("all-restaurants", {
 
                     <!-- Buttons -->
                     <div class="container mb-4 buttons d-inline-flex justify-content-center align-items-center">
-                        <button type="button" class="btn d-sm-flex filters" id="searchButton">search<i
-                                class="fas fa-caret-down p-1"></i></button>
+                        <button type="button" class="btn d-sm-flex filters" id="searchButton" @click="showSearch = !showSearch">search
+                            <i class="fas fa-caret-down p-1"></i></button>
 
-                        <button type="button" class="btn d-sm-flex filters" id="filterButton">filter<i
-                                class="fas fa-caret-down p-1"></i></i></button>
+                        <button type="button" class="btn d-sm-flex filters" id="filterButton" @click="showFilter = !showFilter">filter
+                            <i class="fas fa-caret-down p-1"></i></button>
                         
-                        <button type="button" class="btn d-sm-flex filters" id="sortButton">sort<i
-                                class="fas fa-caret-down p-1"></i></i></button>
+                        <button type="button" class="btn d-sm-flex filters" id="sortButton" @click="showSort = !showSort">sort
+                            <i class="fas fa-caret-down p-1"></i></button>
                     </div>
                     <!-- End of buttons -->
 
                     <!-- Search -->
-                    <div class="container mb-3 searchInput" style="display: none; width: 85%;">
-                        <div class="row g-4 justify-content-between align-items-center d-md-flex">
-                            <div class="col-md">
-                                <input type="text" class="form-control" placeholder="Name" aria-label="Name">
-                            </div>
-                            <div class="col-md">
-                                <input type="text" class="form-control" placeholder="Type" aria-label="Type">
-                            </div>
-                            <div class="col-md">
-                                <input type="text" class="form-control" placeholder="Location" aria-label="Location">
-                            </div>
-                            <div class="col-md">
-                                <select class="form-select" placeholder="Type" aria-label="Type">
-                                    <option value="1">1</option>
-                                    <option value="2">2</option>
-                                    <option value="3">3</option>
-                                    <option value="4">4</option>
-                                    <option value="5">5</option>
-                                </select>
-                            </div>
-                            <div class="col-md p-0">
-                                <button type="button" class="btn btn-sm search-button"><i
-                                        class="fa fa-search me-2"></i>Search</button>
+                    <transition name="show">
+                        <div class="container mb-3 searchInput" style="width: 85%;" v-if="showSearch">
+                            <div class="row g-4 justify-content-between align-items-center d-md-flex">
+                                <div class="col-md">
+                                    <input type="text" class="form-control" placeholder="Name" aria-label="Name">
+                                </div>
+                                <div class="col-md">
+                                    <input type="text" class="form-control" placeholder="Type" aria-label="Type">
+                                </div>
+                                <div class="col-md">
+                                    <input type="text" class="form-control" placeholder="Location" aria-label="Location">
+                                </div>
+                                <div class="col-md">
+                                    <select class="form-select" placeholder="Type" aria-label="Type">
+                                        <option value="1">1</option>
+                                        <option value="2">2</option>
+                                        <option value="3">3</option>
+                                        <option value="4">4</option>
+                                        <option value="5">5</option>
+                                    </select>
+                                </div>
+                                <div class="col-md p-0">
+                                    <button type="button" class="btn btn-sm search-button"><i
+                                            class="fa fa-search me-2"></i>Search</button>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    </transition>
                     <!-- End of search -->
 
                     <!-- Filter -->
-                    <div style="display:none; width: 85%;" class="container mb-3 filterInput">
-                        <div class="row justify-content-center align-items-center d-md-flex search-input">
-                            <div class="col-md">
-                                <select class="form-select" placeholder="Order status" aria-label="Order status">
-                                    <option value="" disabled selected hidden>Restaurant type</option>
-                                    <option value="italian">Italian</option>
-                                    <option value="fast-food">Fast food</option>
-                                </select>
-                            </div>
+                    <transition name="show">
+                        <div style="width: 85%;" class="container mb-3 filterInput" v-if="showFilter">
+                            <div class="row justify-content-center align-items-center d-md-flex search-input">
+                                <div class="col-md">
+                                    <select class="form-select" placeholder="Order status" aria-label="Order status">
+                                        <option value="" disabled selected hidden>Restaurant type</option>
+                                        <option value="italian">Italian</option>
+                                        <option value="fast-food">Fast food</option>
+                                    </select>
+                                </div>
 
-                            <div class="col-md mt-2">
-                                <div class="form-check form-check-inline">
-                                    <input class="form-check-input" type="radio" name="opened" value="" id="flexCheck">
-                                    <label class="form-check-label" for="flexCheckDefault">Show only opened restaurants</label>
+                                <div class="col-md mt-2">
+                                    <div class="form-check form-check-inline">
+                                        <input class="form-check-input" type="radio" name="opened" value="" id="flexCheck">
+                                        <label class="form-check-label" for="flexCheckDefault">Show only opened restaurants</label>
+                                    </div>
+                                </div>
+                    
+                                <div class="col-md mt-1">
+                                    <button type="button" class="btn btn-sm filter-button">Filter</button>
                                 </div>
                             </div>
-                
-                            <div class="col-md mt-1">
-                                <button type="button" class="btn btn-sm filter-button">Filter</button>
-                            </div>
                         </div>
-                    </div>
+                    </transition>
                     <!-- End of filter -->
 
                     <!-- Sort -->
-                    <div class="container mb-3 sortInput" style="display: none; width: 85%;">
-                        <div class="row g-4 justify-content-between align-items-center d-md-flex">
-                            <div class="col-md-3 me-5 select">
-                                <select class="form-select" aria-label="Sort by" placeholder="Sort by">
-                                    <option value="" disabled selected hidden>Sort by</option>
-                                    <option value="1">Restaurant name</option>
-                                    <option value="2">Location</option>
-                                    <option value="3">Rating</option>
-                                </select>
-                            </div>
-            
-                            <div class="col-md-3 p-2 d-inline-flex">
-                                <div class="form-check form-check-inline">
-                                    <input class="form-check-input" type="radio" name="ascOrDsc" value="" id="flexCheck">
-                                    <label class="form-check-label" for="flexCheckDefault">Ascending</label>
+                    <transition name="show">
+                        <div class="container mb-3 sortInput" style="width: 85%;" v-if="showSort">
+                            <div class="row g-4 justify-content-between align-items-center d-md-flex">
+                                <div class="col-md-3 me-5 select">
+                                    <select class="form-select" aria-label="Sort by" placeholder="Sort by">
+                                        <option value="" disabled selected hidden>Sort by</option>
+                                        <option value="1">Restaurant name</option>
+                                        <option value="2">Location</option>
+                                        <option value="3">Rating</option>
+                                    </select>
                                 </div>
-            
-                                <div class="form-check form-check-inline">
-                                    <input class="form-check-input" type="radio" name="ascOrDsc" value="" id="flexCheck">
-                                    <label class="form-check-label" for="flexCheckDefault">Descending</label>
+                
+                                <div class="col-md-3 p-2 d-inline-flex">
+                                    <div class="form-check form-check-inline">
+                                        <input class="form-check-input" type="radio" name="ascOrDsc" value="" id="flexCheck">
+                                        <label class="form-check-label" for="flexCheckDefault">Ascending</label>
+                                    </div>
+                
+                                    <div class="form-check form-check-inline">
+                                        <input class="form-check-input" type="radio" name="ascOrDsc" value="" id="flexCheck">
+                                        <label class="form-check-label" for="flexCheckDefault">Descending</label>
+                                    </div>
                                 </div>
-                            </div>
-            
-                            <div class="col-md p-0">
-                                <button type="button" class="btn btn-sm filter-button">Sort</button>
+                
+                                <div class="col-md p-0">
+                                    <button type="button" class="btn btn-sm filter-button">Sort</button>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    </transition>
                     <!-- End of sort -->
 
                 </div>
