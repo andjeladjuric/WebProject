a80b434f942bcdf9693bb5c0408e126bc4dc65d1
jovicpetra <jovic.petra@uns.ac.rs>
2021-08-19 13:06:19 +0200

    new restaurant form view done

--- FoodDeliveryApp/WebContent/app/administrator/components/newRestaurant.js
+++ FoodDeliveryApp/WebContent/app/administrator/components/newRestaurant.js
@@ -3,6 +3,7 @@ Vue.component("restaurant-form",{
         return{
 				step : 1,
 				totalSteps : 3,
+				table : 1
             
         }
     }
@@ -13,7 +14,7 @@ Vue.component("restaurant-form",{
         <div class="row">
             <div class="col-lg-6 col-sm-11 mx-auto">
                 <div class="card shadow-sm my-3 align-items-center" style="height: 600px;">
-                    <form>
+                    <form style="height: 520px;">
 						<section v-if="step == 1">
                                 <div class="d-flex justify-content-center">
                                     <h4 class="card-title my-5">Restaurant Informations</h4>
@@ -198,17 +199,137 @@ Vue.component("restaurant-form",{
                         </section>
 
                         <section v-if="step == 3">
-							<div class="d-flex justify-content-center">
-                                <h4 class="card-title my-5">Choose Manager</h4>
-                            </div>
-                            
-						</section>
-						<div class="d-flex justify-content-evenly">
+                                 <section v-if="table == 1">
+                                    <div class="d-flex justify-content-center">
+                                        <h4 class="card-title my-5">Choose Manager</h4>
+                                    </div>
+                                    <div class="d-grid gap-3">
+                                        <div class="row justify-content-center">
+                                            <div class="col-5 mx-auto">
+                                                <div class="input-group mb-3">
+                                                    <input type="text" class="form-control">
+                                                    <button class="btn btn-outline-secondary" type="button"><i
+                                                            class="fas fa-search"></i></button>
+                                                </div>
+                                            </div>
+                                            <div class="col-5 mx-auto">
+                                                <div class="input-group mb-3">
+                                                    <label class="input-group-text">Sort By</label>
+                                                    <select class="form-select">
+                                                        <option selected></option>
+                                                        <option value="1">First Name</option>
+                                                        <option value="2">Last Name</option>
+                                                        <option value="3">Username</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div class="row">
+                                            <div class="col-10 mx-auto">
+                                                <div style="height:250px;overflow:auto;">
+                                                    <table class="table table-sm caption-top table-hover">
+                                                        <thead>
+                                                            <tr>
+                                                                <th scope="col">Name</th>
+                                                                <th scope="col">Surname</th>
+                                                                <th scope="col">Username</th>
+                                                            </tr>
+                                                        </thead>
+                                                        <tbody>
+                                                        </tbody>
+                                                    </table>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div class="row">
+                                            <div class="col-6 justify-content-center">
+                                                <a href="" @click.prevent="newManager">
+                                                    Create New Menager
+                                                </a>
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                </section>
+
+                                <section v-if="table == 0">
+                                    <form>
+                                        <div class="d-flex justify-content-center">
+                                            <h4 class="card-title my-5">New Manager</h4>
+                                        </div>
+                                        <div class="d-grid gap-3">
+                                            <div class="row">
+                                                <div class="col-3 mx-auto">
+                                                    <label class="form-label">Name:</label>
+                                                </div>
+                                                <div class="col-8  mx-auto">
+                                                    <input type="text" class="form-control">
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                                <div class="col-3 mx-auto">
+                                                    <label class="form-label">Surname:</label>
+                                                </div>
+                                                <div class="col-8  mx-auto">
+                                                    <input type="text" class="form-control">
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                                <div class="col-3 mx-auto">
+                                                    <label class="form-label">Username:</label>
+                                                </div>
+                                                <div class="col-8  mx-auto">
+                                                    <input type="text" class="form-control">
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                                <div class="col-3 mx-auto">
+                                                    <label class="form-label">Password:</label>
+                                                </div>
+                                                <div class="col-8  mx-auto">
+                                                    <input type="password" class="form-control">
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                                <div class="col-3 mx-auto">
+                                                    <label class="form-label">Date Of Birth:</label>
+                                                </div>
+                                                <div class="col-8  mx-auto">
+                                                    <input type="date" class="form-control">
+                                                </div>
+                                            </div>
+                                            <div class="d-flex gap-2 justify-content-evenly ">
+                                                <div class="form-check">
+                                                    <input class="form-check-input" type="radio">
+                                                    <label class="form-check-label">
+                                                        Male
+                                                    </label>
+                                                </div>
+                                                <div class="form-check">
+                                                    <input class="form-check-input" type="radio" checked>
+                                                    <label class="form-check-label">
+                                                        Female
+                                                    </label>
+                                                </div>
+                                            </div>
+                                            <div class="d-flex justify-content-evenly">
+                                                <button class="btn buttonGroup"  @click.prevent="cancel">Cancel</button>
+                                                <button class="btn buttonGroup" @click.prevent="save">Save</button>
+                                            </div>
+                                        </div>
+                                    </form>
+
+                                </section>
+                            </section>
+                    </form>
+
+					<div class="d-flex gap-5" v-if="table == 1">
 						<button v-if="step != 1" class="btn buttonGroup" @click.prevent="prevStep">Prev</button>
 						<button v-if="step != totalSteps" class="btn buttonGroup" @click.prevent="nextStep">Next</button>
-						<button v-if="step == totalSteps" class="btn buttonGroup" @click.prevent="submit">Create</button>
+						<button v-if="step == totalSteps" class="btn buttonGroup" @click.prevent="submit">Submit</button>
 					</div>
-                    </form>
 					
                 </div>
             </div>
@@ -226,6 +347,14 @@ Vue.component("restaurant-form",{
 		prevStep : function()
 		{
 			this.step--;
+		},
+		newManager : function()
+		{
+			this.table = 0;
+		},
+		cancel : function()
+		{
+			this.table = 1;
 		}
 		
     },
